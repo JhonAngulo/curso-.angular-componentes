81e2fd6f24e24ae6bdc867f0350e6997c74bff41
Jhon Angulo <jhon.angulo@jikkosoft.com>
2021-11-30 19:47:50 -0500

    add global store

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../models/product.model'
+import { Product } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  private myShoppingCart: Product[] = []
+  private myShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
+
+  myCart$ = this.myCart.asObservable();
 
   constructor() { }
 
-  getShoppingCart() {
-    return this.myShoppingCart;
-  }
-
   addProduct(product: Product) {
     this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart)
+  }
+
+  getShoppingCart() {
+    return this.myShoppingCart;
   }
 
   getTotal() {
